98e0913887033aa9e1b13f93279cfbe00c7077e7
Alyssasorensen <-m>
2023-05-06 17:49:20 +0000

    added a main.py file

---
+++ data/main.py
@@ -0,0 +1,14 @@
+from flask import Flask
+import pandas as pd
+
+df = pd.read_csv('./data/data.csv')
+
+app = Flask(__name__)
+
+@app.route('/', methods='get')
+def home():
+    return 'this is an API service for MN ICD code details'
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
